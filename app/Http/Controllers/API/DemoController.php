0fe6a7013253f23038649ed02bbc8f2670cba657
yls <yangliansheng_zz@163.com>
2018-09-11 10:36:09 +0800

    项目基础文件提交

---
+++ app/Http/Controllers/API/DemoController.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yls-demo
+ * Date: 2018/6/27
+ * Time: 16:48
+ */
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+
+class DemoController extends Controller
+{
+    public function index() {
+        echo 'demo-action';
+        DB::insert('insert into users (id, name) values (?, ?)', [1, '学院君']);
+    }
+
+}
